b5d46c7bf4935a5ca130c036bdb98eb191ee6387
Aifia Mohamed Amine <aifiamohamedamine@isamm.u-manouba.tn>
2024-07-17 21:42:18 +0100

    chore: Add user module and migration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MigrationModule } from './migration/migration.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        `.env.${process.env.NODE_ENV}`,
+        '.env',
+      ],
+      isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads/',
+    }),
+    MongooseModule.forRoot("mongodb://localhost:27017/migrationAppDB"),
+    MigrationModule, 
+    AuthModule, 
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+   .setTitle('Migration App API')
+  .setDescription('API for Migration App')
+  .setVersion('1.0')
+  .addTag('Migration backend API')
+  .addBearerAuth()
+  .build();
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+
+  console.log("--------------------------------------------------------------------------------------");
+  console.log(`Server running on port ${PORT} and you can access the API at http://localhost:${PORT}/api`);
+  console.log("--------------------------------------------------------------------------------------");
+  console.log(`Swagger documentation available at http://localhost:${PORT}/api`);
+  console.log("--------------------------------------------------------------------------------------");
+  console.log(`MongoDB connection string: ${process.env.MONGODB_URI}`);
+  console.log("--------------------------------------------------------------------------------------");
 }
 bootstrap();
