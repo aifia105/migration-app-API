dea08a91dbb13342825a94bb1c5aa7a964556ffc
Aifia Mohamed Amine <aifiamohamedamine@isamm.u-manouba.tn>
2024-07-19 22:51:07 +0100

    chore: Remove unused user and auth modules and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MigrationModule } from './migration/migration.module';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
@@ -22,10 +20,8 @@ import { MongooseModule } from '@nestjs/mongoose';
       rootPath: join(__dirname, '..', 'uploads'),
       serveRoot: '/uploads/',
     }),
-    MongooseModule.forRoot("mongodb://localhost:27017/migrationAppDB"),
-    MigrationModule, 
-    AuthModule, 
-    UserModule],
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    MigrationModule, ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT ;
   await app.listen(PORT);
 
   console.log("--------------------------------------------------------------------------------------");
